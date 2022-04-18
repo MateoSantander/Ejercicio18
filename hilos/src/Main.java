13f275654998835a0a8997b9b73996ec62a61db4
Mateo Rodriguez <mateo.rsantander@alumnos.upm.es>
2022-04-18 14:24:22 +0200

    finalizado

---
+++ hilos/src/Main.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        Runnable primo1 = () -> {
+            System.out.println("Escriba un numero: ");
+            int numero1 = sc.nextInt();
+            int contador = 2;
+            boolean primo=true;
+            while ((primo) && (contador!=numero1)){
+                if (numero1 % contador == 0)
+                    primo = false;
+                contador++;
+            }
+            if(primo)
+                System.out.println("ES primo");
+            else
+                System.out.println("NO es primo");
+        };
+        Thread t1 = new Thread(primo1);
+        t1.start();
+    }
+}
+
